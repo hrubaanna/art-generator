06d61756e047e2c4fcfe4c7118db7ca0f49ce6ba
Anna Hruba <hruba.skoly@gmail.com>
2022-09-12 14:48:14 +0200

    dalle component selecting photo and other photos dissapear

--- src/pages/testPage.js
+++ src/pages/testPage.js
@@ -2,6 +2,22 @@ import React from "react";
 import Image from "next/image";
 
 class TestPage extends React.Component {
+  showFavorite = (e) => {
+    let img = e.target;
+    document.querySelectorAll("img").forEach((photo) => {
+      if (photo.id != e.target.id) {
+        console.log(photo);
+        photo.style.display = "none";
+      }
+    });
+    img.setAttribute(
+      "style",
+      "width: 5em; height: 5em; margin: auto; display: block;"
+    );
+    img.className = "favorite-img";
+    document.querySelector(".card").appendChild(img);
+  };
+
   render() {
     return (
       <div>
@@ -9,7 +25,9 @@ class TestPage extends React.Component {
           {/* TODO: test removing photo as in Dalle Component, test changing signature color to inverted */}
 
           <Image
+            onClick={this.showFavorite}
             id="0"
+            value="0"
             src="/TestPhotos/DALLE_2.png"
             width="500em"
             height="500em"
