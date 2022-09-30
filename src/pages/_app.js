d7bed016ba171665a07420fd1233861d843fb9ef
David Kubánek <60887837+davidkubanek@users.noreply.github.com>
2022-09-30 14:40:10 +0100

    fixed Head meta tag warning

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,18 @@
 import "../styles/globals.css";
+import Head from "next/head";
 
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, height=device-height, target-densitydpi=device-dpi, initial-scale=1.0, minimum-scale=1, maximum-scale=1.0, user-scalable=no"
+        />
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default App;
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,10 +10,6 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <meta
-            name="viewport"
-            content="width=device-width, height=device-height, target-densitydpi=device-dpi, initial-scale=1.0, minimum-scale=1, maximum-scale=1.0, user-scalable=no"
-          />
           <link
             href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@1,300&display=swap"
             rel="stylesheet"
