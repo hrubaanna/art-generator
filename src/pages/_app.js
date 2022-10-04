fc4a6ac3e3abfdd010cd7a4e86c0ae29b81de0d1
Anna Hruba <hruba.skoly@gmail.com>
2022-10-04 15:35:30 +0100

    removed random semi-col lingering on all pages

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -10,7 +10,7 @@ function App({ Component, pageProps }) {
           content="width=device-width, height=device-height, initial-scale=1.0, minimum-scale=1, maximum-scale=1.0, user-scalable=no"
         />
       </Head>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </>
   );
 }
